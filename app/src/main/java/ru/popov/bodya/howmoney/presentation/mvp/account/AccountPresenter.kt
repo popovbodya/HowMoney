0a881936b85adb1b08f77a027151e4c7f2576a42
Popov Bogdan <popovbodya@gmail.com>
2018-07-24 16:31:10 +0300

    немного поменял логику получения стабового счета
    (cherry picked from commit 5ddfe59)

--- app/src/main/java/ru/popov/bodya/howmoney/presentation/mvp/account/AccountPresenter.kt
+++ app/src/main/java/ru/popov/bodya/howmoney/presentation/mvp/account/AccountPresenter.kt
@@ -1,13 +1,15 @@
 package ru.popov.bodya.howmoney.presentation.mvp.account
 
 import com.arellomobile.mvp.InjectViewState
+import io.reactivex.Observable
+import ru.popov.bodya.core.extensions.connect
 import ru.popov.bodya.core.mvp.AppPresenter
 import ru.popov.bodya.core.rx.RxSchedulersTransformer
 import ru.popov.bodya.howmoney.domain.account.interactors.CurrencyInteractor
+import ru.popov.bodya.howmoney.domain.account.models.Currency
 import ru.popov.bodya.howmoney.presentation.ui.global.Screens
 import ru.terrakok.cicerone.Router
 import javax.inject.Inject
-
 
 /**
  *  @author popovbodya
@@ -19,6 +21,20 @@ class AccountPresenter @Inject constructor(
         private val router: Router
 ) : AppPresenter<AccountView>() {
 
+    override fun onFirstViewAttach() {
+        super.onFirstViewAttach()
+        val disposable = Observable.merge(
+                currencyInteractor.getCurrentCurrencyAmount(Currency.RUB).toObservable(),
+                currencyInteractor.getCurrentCurrencyAmount(Currency.USD).toObservable())
+                .compose(rxSchedulersTransformer.ioToMainTransformer())
+                .subscribe {
+                    when (it.currency) {
+                        Currency.RUB -> viewState.showRUBAmount(it.amount)
+                        Currency.USD -> viewState.showUSDAmount(it.amount)
+                    }
+                }
+                .connect(compositeDisposable)
+    }
 
     fun onAboutMenuItemClick() {
         router.navigateTo(Screens.ABOUT_SCREEN)
