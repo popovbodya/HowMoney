bad9b7cfe9297b8a983f27c8f6e7405415ef0a30
Lounah <lounahxrussia@gmail.com>
2018-08-06 02:56:43 +0300

    sharedprefswrapper now can save fav currency

--- app/src/main/java/ru/popov/bodya/howmoney/data/database/preferences/SharedPreferencesWrapper.kt
+++ app/src/main/java/ru/popov/bodya/howmoney/data/database/preferences/SharedPreferencesWrapper.kt
@@ -9,11 +9,21 @@ import ru.popov.bodya.howmoney.data.network.beans.CurrentRateBean
 class SharedPreferencesWrapper(private val sharedPreferences: SharedPreferences) {
 
     private companion object {
+        const val FAV_CURRENCY_KEY = "FAV_CURRENCY_KEY"
+        const val DEFAULT_CURRENCY = "RUB"
         const val EXCHANGE_RATE_KEY = "currency_type"
         const val DEFAULT_RATE = "60"
     }
 
     fun getExchangeRate(): String = sharedPreferences.getString(EXCHANGE_RATE_KEY, DEFAULT_RATE)
+
+    fun getFavExchangeRate(): String = sharedPreferences.getString(FAV_CURRENCY_KEY, DEFAULT_CURRENCY)
+
+    fun setNewFavCurrency(currency: String) {
+        val editor = sharedPreferences.edit()
+        editor.putString(FAV_CURRENCY_KEY, currency)
+        editor.apply()
+    }
 
     fun saveExchangeRate(currentRateBean: CurrentRateBean) {
         val editor = sharedPreferences.edit()
