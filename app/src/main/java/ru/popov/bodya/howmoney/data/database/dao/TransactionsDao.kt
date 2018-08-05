2cb139adfda2bf13b2a4ba686138abf87e3887eb
Lounah <lounahxrussia@gmail.com>
2018-08-05 05:03:27 +0300

    now dao returns flowable instead of single

--- app/src/main/java/ru/popov/bodya/howmoney/data/database/dao/TransactionsDao.kt
+++ app/src/main/java/ru/popov/bodya/howmoney/data/database/dao/TransactionsDao.kt
@@ -2,26 +2,41 @@ package ru.popov.bodya.howmoney.data.database.dao
 
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Query
-import io.reactivex.Single
+import io.reactivex.Flowable
 import ru.popov.bodya.howmoney.domain.wallet.models.Transaction
 
 @Dao
 interface TransactionsDao : BaseDao<Transaction> {
     @Query("SELECT * FROM transactions")
-    fun getAllTransactions(): Single<List<Transaction>>
+    fun getAllTransactions(): Flowable<List<Transaction>>
 
     @Query("SELECT * FROM transactions WHERE amount>0")
-    fun getAllIncomeTransactions(): Single<List<Transaction>>
+    fun getAllIncomeTransactions(): Flowable<List<Transaction>>
+
+    @Query("SELECT SUM(amount) FROM transactions WHERE walletId=:walletId AND amount>0 ")
+    fun getAllIncomeTransactionsSumByWalletId(walletId: Int): Flowable<Double>
+
+    @Query("SELECT SUM(amount) FROM transactions WHERE walletId=:walletId AND amount<0 ")
+    fun getAllExpenseTransactionsSumByWalletId(walletId: Int): Flowable<Double>
 
     @Query("SELECT * FROM transactions WHERE amount<0")
-    fun getAllExpenseTransactions(): Single<List<Transaction>>
+    fun getAllExpenseTransactions(): Flowable<List<Transaction>>
 
     @Query("SELECT * FROM transactions WHERE walletId=:walletId")
-    fun getAllTransactionsByWalletId(walletId: Int): Single<List<Transaction>>
+    fun getAllTransactionsByWalletId(walletId: Int): Flowable<List<Transaction>>
 
     @Query("SELECT * FROM transactions WHERE walletId=:walletId AND amount>0")
-    fun getAllIncomeTransactionsByWalletId(walletId: Int): Single<List<Transaction>>
+    fun getAllIncomeTransactionsByWalletId(walletId: Int): Flowable<List<Transaction>>
 
     @Query("SELECT * FROM transactions WHERE walletId=:walletId AND amount<0")
-    fun getAllExpenseTransactionsByWalletId(walletId: Int): Single<List<Transaction>>
+    fun getAllExpenseTransactionsByWalletId(walletId: Int): Flowable<List<Transaction>>
+
+    @Query("DELETE FROM transactions WHERE walletId=:walletId")
+    fun deleteAllTransactionsByWalletId(walletId: Int)
+
+    @Query("DELETE FROM transactions WHERE id=:transactionId")
+    fun deleteTransactionById(transactionId: Int)
+
+    @Query("SELECT * FROM transactions WHERE id=:transactionId")
+    fun getTransactionById(transactionId: Int): Flowable<Transaction>
 }
--- app/src/main/java/ru/popov/bodya/howmoney/data/database/dao/WalletDao.kt
+++ app/src/main/java/ru/popov/bodya/howmoney/data/database/dao/WalletDao.kt
@@ -2,20 +2,23 @@ package ru.popov.bodya.howmoney.data.database.dao
 
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Query
-import io.reactivex.Single
+import io.reactivex.Flowable
 import ru.popov.bodya.howmoney.domain.wallet.models.Wallet
 
 @Dao
 interface WalletDao : BaseDao<Wallet> {
     @Query("SELECT * FROM wallets")
-    fun getAllWallets(): Single<List<Wallet>>
+    fun getAllWallets(): Flowable<List<Wallet>>
 
     @Query("SELECT * FROM wallets WHERE id=:id")
-    fun getWalletById(id: Int): Single<Wallet>
+    fun getWalletById(id: Int): Flowable<Wallet>
 
     @Query("UPDATE wallets SET amount=:newBalance WHERE id=:walletId")
     fun updateWalletBalance(walletId: Int, newBalance: Double)
 
-    @Query("UPDATE wallets SET amount=amount+:inc WHERE id=:walletId")
+    @Query("UPDATE wallets SET amount=(amount+:inc) WHERE id=:walletId")
     fun increaseWalletBalance(walletId: Int, inc: Double)
+
+    @Query("DELETE FROM wallets WHERE id=:walletId")
+    fun deleteWalletById(walletId: Int)
 }
